eac4a88e33e6175c8dd8d5b34ee33f8a1f8250b2
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-02-27 22:28:34 +0700

    add list endpoint for blogs

--- src/controllers/blog.ts
+++ src/controllers/blog.ts
@@ -33,10 +33,10 @@ export const createBlog = (req: Request, res: Response) => {
     const newBlog = new Blog({
       title,
       content,
-      slug: slugify(title as string).toString(),
+      slug: slugify(title as string).toLowerCase(),
       metaTitle: process.env.APP_NAME || "Blogify",
       metaDescription: stripHtml((content as string).substring(0, 160)).result,
-      tags: [],
+      tags: tags ?? [],
       excerpt: trimExcerpt(content as string, 320, ' ', '...'),
       author: (req as any).profile._id
     });
@@ -50,7 +50,7 @@ export const createBlog = (req: Request, res: Response) => {
       }
 
       (newBlog as any).featuredImage.data = readFileSync((files.featuredImage as File).path);
-      (newBlog as any).featuredImage.contentType = readFileSync((files.featuredImage as File).type)
+      (newBlog as any).featuredImage.contentType = (files.featuredImage as File).type;
     }
 
     try {
@@ -62,10 +62,44 @@ export const createBlog = (req: Request, res: Response) => {
       })
     } catch (error) {
       return res.status(400).json({
-        error,
         success: false,
         message: customMongoErrorHandler(error)
       })
     }
   })
+}
+
+export const listBlogs = async (req: Request, res: Response) => {
+  const { limit = 10, search = "" } = req.query;
+  // return res.json({
+  //   success: false,
+  //   message: "customMongoErrorHandler(err)"
+  // });
+
+  try {
+    const blogs = await Blog.find({
+      $or: [{
+        title: {
+          $regex: search,
+          $options: "i"
+        }
+      }]
+    })
+      .populate("tags", "_id name slug")
+      .populate("author", "_id name username email")
+      .select("_id title content createdAt updatedAt author tags excerpt")
+      .limit(+limit);
+    return res.json({
+      success: true,
+      data: blogs,
+    })
+
+  } catch (err) {
+    console.log("🚀 ~ file: blog.ts ~ line 103 ~ listBlogs ~ err", err)
+    return res.status(400)
+      .json({
+        success: false,
+        message: customMongoErrorHandler(err)
+      })
+  }
 }
--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { authMiddleware, requireSignin } from "../controllers/auth";
-import { createBlog } from "../controllers/blog";
-
+import { createBlog, listBlogs } from "../controllers/blog";
 
 export const BlogRoutes = Router();
 
 BlogRoutes.post('/create', requireSignin, authMiddleware, createBlog);
+BlogRoutes.get('/list', listBlogs);
